9b3a69cbe4f6e091b71776e4b132f8b696f69236
UWIMANA Jean Paul <uwimanajeanpaul3@gmail.com>
2019-08-08 10:58:30 +0200

    (models):adding files containing arrays[Finishes #167774316]

---
+++ server/models/user_file.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        user_id:1,
+        email:"paul@gmail.com",
+        password:"paul12345",
+        first_name:"uwimana",
+        last_name:"paul",
+        is_admin:"true"
+},
+{
+        user_id:2,
+        email:"karl@gmail.com",
+        password:"karl12345",
+        first_name:"karl",
+        last_name:"peter",
+        is_admin:"false"
+}
+
+];
+export default users;
