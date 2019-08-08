f5b0a9de6b43cfd452b1e8a01928aa7f04cc54bb
UWIMANA Jean Paul <uwimanajeanpaul3@gmail.com>
2019-08-08 14:57:31 +0200

    (cancel trip):admin can cancel trip[Finishes #167784244]

---
+++ server/controllers/updateTrip.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import Joi from 'joi';
+
+import trips from '../models/trip_file';
+
+import tripValidation from '../helpers/trip_validation'
+    
+const update_trip= (req, res)=>{
+   
+const id = trips.filter(id=>id.trip_id === parseInt(req.params.trip_id));
+if(!id){
+    return res.status(404).send({
+        status:"error",
+        error:"trip not found"
+    })
+}
+const valid = {
+    status:Joi.string().required()
+}
+const validated = Joi.validate(req.body,valid)
+if(validated.error)
+{
+    return res.status(400).send(validated.error.details[0].message);
+}
+id.status=req.body.status
+if(req.body.status === "cancelled")
+{
+    return res.status(200).json({
+        status:"success",
+    message:'trip cancelled successfully'
+    })
+}
+return res.status(400).send({
+    status:"error",
+    error:"you should enter cancelled to cancel trip"
+})
+}
+export default  update_trip;
+
+
